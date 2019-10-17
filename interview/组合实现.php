2e0e4eba60247d15f00325a101d005ca72154103
JsonHe <13524519563@163.com>
2019-10-17 14:05:30 +0800

    2

---
+++ interview/组合实现.php
@@ -0,0 +1,25 @@
+<?php
+
+//str_split()  字符串转数组
+$a = ['a','b','c'];
+$b = get_combinations($a);
+echo json_encode($b);
+
+function get_combinations($str, &$comb = [])
+{
+    if (count($str) > 1) {
+        $str_first = array_shift($str);
+        $comb_temp = get_combinations($str, $comb);
+        $comb[] = [$str_first];
+        foreach ($comb_temp as $k => $v) {
+            $item = $v;
+            $item[] = $str_first;
+            $comb[] = $item;
+        }
+    } else {
+        $comb[] = $str;
+    }
+
+    return $comb;
+}
+
