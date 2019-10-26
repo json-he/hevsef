cf69e7cfa0aee4a2c027cef9f302c5a25a9fd5e0
jsonHe <827727413@qq.com>
2019-10-26 20:10:22 +0800

    2

---
+++ html/秒味课堂/《解惑Vue组件之间的通信》公开课课件/src--准备/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Modal></Modal>
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/modal.vue'
+import TodoList from './components/todolist/todolist.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Modal,
+    TodoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ html/秒味课堂/《解惑Vue组件之间的通信》公开课课件/src2/components/todolist/todo-header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="header" >
+      <h1>todos</h1>
+      <input class="new-todo" placeholder="请输入内容" />
+  </header>
+</template>
+<script>
+  export default {
+    name: 'todoHeader'
+  }
+</script>
