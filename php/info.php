6f78bb69af61d243d14a423e6e511ebfc957365b
JsonHe <13524519563@163.com>
2019-11-13 10:36:46 +0800

    2

--- php/info.php
+++ php/info.php
@@ -1,5 +1,5 @@
 <?php
-
     $r = 11;
     $s = 22;
-  phpinfo();
+    $str = "https://jupiterbucket2.oss-cn-hangzhou.aliyuncs.com/cindy_2015/material/10027/安盛300*250.jpg";
+    file_get_contents($str);
