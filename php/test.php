a77d946fc85ca8c325274e2d257cb735ab03dced
Sylvain-ssy <13524519563@163.com>
2020-05-26 15:21:04 +0800

    6

--- php/test.php
+++ php/test.php
@@ -1,12 +1,4 @@
 <?php
- $begin = 0;
- $end = 47;
- $input = range($begin,$end);
-$out = [];
-for($i=0;$i<7;$i++){
-    foreach ($input as $v){
-        $out[] = $i*24+ 0.5*$v;
-    }
-}
+$s = null;
 
-print_r($out);
+var_dump(isset($s));
