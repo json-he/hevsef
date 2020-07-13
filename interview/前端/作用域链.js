36538f85fdf963e5a7b2367cea1e7d588317b799
Sylvain-ssy <13524519563@163.com>
2020-07-13 15:34:13 +0800

    转移

---
+++ interview/前端/作用域链.js
@@ -0,0 +1,10 @@
+function fun() {
+    console.log(1);
+}
+fun();
+function fun() {
+    console.log(2);
+}
+fun();
+// 2
+// 2
--- php/test.php
+++ php/test.php
@@ -1,2 +1,10 @@
 <?php
- echo intval(0.58*100);
+$s1 = extension_loaded('imagick');
+$s2 = extension_loaded('gd');
+
+var_dump($s1);
+var_dump($s2);
+
+// phpinfo();
+
+
